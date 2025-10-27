f762ce45dfe53170161b186b3d9e8f9b6cbfdd1c
Shivxnshjasathi <shivansh.jasathi.aiml21@ggits.net>
2025-10-28 02:40:48 +0530

    Initial Commit

---
+++ VakeelNow/VakeelNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  VakeelNowApp.swift
+//  VakeelNow
+//
+//  Created by Shivansh Jasathi on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct VakeelNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
